a1d83b0aa02706212fe51c72eddea099bc81b3dd
Brian Roos <onlinedepartment@Dev-station.local>
2018-04-23 15:08:41 +0200

    Navigation update (extra menuitem)

--- app/index.php
+++ app/index.php
@@ -1,6 +1,7 @@
 <?php
   $compiler = include('compiler.php');
   $data['title'] = 'Index';
+  $data['search'] = true;
 
   echo $compiler->render('index', $data);
 ?>
--- app/scripts/modules.js
+++ app/scripts/modules.js
@@ -222,10 +222,12 @@ function updateDatetime() {
   var div = $('.navigation .datetime');
 
   var d = new Date();
+  var days = ['zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag'];
   var months = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december'];
   var time = checkZero(d.getHours()) + ':' + checkZero(d.getMinutes());
 
   var date = [d.getDate(), months[d.getMonth()], d.getFullYear()].join(' ');
+  date = [days[d.getDay()], date].join(', ');
   div.html('<p>'+ [date, time].join(' / ') +'</p>');
 
   // Repeat function every second
